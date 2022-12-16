ae010e64a13dcfd9af4d27386426f5f2a91cad4c
Junya-0220 <junya010220@gmail.com>
2022-12-16 22:46:06 +0900

    ログインフォームの実装

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,38 @@
+import { useState } from "react";
+import Input from "./form/Input";
+
 const Login = () => {
+  const [email, setEmail] = useState();
+  const [passWord, setPassWord] = useState();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <div className="text-center">
+    <div className="col-md-6 offset-md-3">
       <h2>Login</h2>
       <hr />
+      <form onSubmit={handleSubmit}>
+        <Input
+          title="Email Address"
+          type="email"
+          className="form-control"
+          name="email"
+          autoComplete="email-new"
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <Input
+          title="Password"
+          type="password"
+          className="form-control"
+          name="password"
+          autoComplete="email-new"
+          onChange={(event) => setPassWord(event.target.value)}
+        />
+        <hr />
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
     </div>
   );
 };
