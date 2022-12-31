41b6933fc36813db29fd8d5af9ad2354451623df
Junya-0220 <junya010220@gmail.com>
2022-12-31 23:51:13 +0900

    refreshtokens commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import Alert from "./components/Alert";
 
@@ -7,7 +7,6 @@ function App() {
   const [alertMessage, setAlertMessage] = useState("");
   const [alertClassName, setAlertClassName] = useState("d-none");
 
-  const [ticking, setTicking] = useState(false);
   const [tickInterval, setTickInterval] = useState();
 
   const navigate = useNavigate();
@@ -16,58 +15,75 @@ function App() {
     const requestOptions = {
       method: "GET",
       credentials: "include",
-    }
+    };
 
     fetch(`/logout`, requestOptions)
-    .catch(error => {
-      console.log("error logging out", error);
-    })
-    .finally(() => {
-      setJwtToken("");
-    })
+      .catch((error) => {
+        console.log("error logging out", error);
+      })
+      .finally(() => {
+        setJwtToken("");
+        toggleRefresh(false);
+      });
 
     navigate("/login");
-  }
+  };
+
+  const toggleRefresh = useCallback(
+    (status) => {
+      console.log("clicked");
+
+      if (status) {
+        console.log("turning on ticking");
+        let i = setInterval(() => {
+          const requestOptions = {
+            method: "GET",
+            credentials: "include",
+          };
+
+          fetch(`/refresh`, requestOptions)
+            .then((response) => response.json())
+            .then((data) => {
+              if (data.access_token) {
+                setJwtToken(data.access_token);
+              }
+            })
+            .catch((error) => {
+              console.log("user is not logged in");
+            });
+        }, 600000);
+        setTickInterval(i);
+        console.log("setting tick interval to", i);
+      } else {
+        console.log("turning off ticking");
+        console.log("turning off tickInterval", tickInterval);
+        setTickInterval(null);
+        clearInterval(tickInterval);
+      }
+    },
+    [tickInterval]
+  );
 
   useEffect(() => {
     if (jwtToken === "") {
       const requestOptions = {
         method: "GET",
         credentials: "include",
-      }
+      };
 
       fetch(`/refresh`, requestOptions)
         .then((response) => response.json())
         .then((data) => {
           if (data.access_token) {
             setJwtToken(data.access_token);
+            toggleRefresh(true);
           }
         })
-        .catch(error => {
+        .catch((error) => {
           console.log("user is not logged in", error);
-        })
+        });
     }
-  }, [jwtToken])
-
-  const toggleRefresh = () => {
-    console.log("clicked");
-
-    if (!ticking) {
-      console.log("turning on ticking");
-      let i  = setInterval(() => {
-        console.log("this will run every second");
-      }, 1000);
-      setTickInterval(i);
-      console.log("setting tick interval to", i);
-      setTicking(true);
-    } else {
-      console.log("turning off ticking");
-      console.log("turning off tickInterval", tickInterval);
-      setTickInterval(null);
-      clearInterval(tickInterval);
-      setTicking(false);
-    }
-  }
+  }, [jwtToken, toggleRefresh]);
 
   return (
     <div className="container">
@@ -134,7 +150,6 @@ function App() {
           </nav>
         </div>
         <div className="col-md-10">
-          <a className="btn btn-outline-secondary" href="#!" onClick={toggleRefresh}>Toggle Ticking</a>
           <Alert message={alertMessage} className={alertClassName} />
           <Outlet
             context={{
@@ -142,6 +157,7 @@ function App() {
               setJwtToken,
               setAlertClassName,
               setAlertMessage,
+              toggleRefresh,
             }}
           />
         </div>
