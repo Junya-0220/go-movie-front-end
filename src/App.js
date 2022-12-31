7ad21c146dfed6719d781495bc8e6d2d9bb2abb8
Junya-0220 <junya010220@gmail.com>
2022-12-31 17:49:20 +0900

    refreshtokensを実装

--- src/App.js
+++ src/App.js
@@ -7,19 +7,34 @@ function App() {
   const [alertMessage, setAlertMessage] = useState("");
   const [alertClassName, setAlertClassName] = useState("d-none");
 
+  const [ticking, setTicking] = useState(false);
+  const [tickInterval, setTickInterval] = useState();
+
   const navigate = useNavigate();
 
   const logOut = () => {
-    setJwtToken("");
+    const requestOptions = {
+      method: "GET",
+      credentials: "include",
+    }
+
+    fetch(`/logout`, requestOptions)
+    .catch(error => {
+      console.log("error logging out", error);
+    })
+    .finally(() => {
+      setJwtToken("");
+    })
+
     navigate("/login");
-  };
+  }
 
   useEffect(() => {
     if (jwtToken === "") {
       const requestOptions = {
         method: "GET",
         credentials: "include",
-      };
+      }
 
       fetch(`/refresh`, requestOptions)
         .then((response) => response.json())
@@ -28,11 +43,31 @@ function App() {
             setJwtToken(data.access_token);
           }
         })
-        .catch((error) => {
+        .catch(error => {
           console.log("user is not logged in", error);
-        });
+        })
     }
-  }, [jwtToken]);
+  }, [jwtToken])
+
+  const toggleRefresh = () => {
+    console.log("clicked");
+
+    if (!ticking) {
+      console.log("turning on ticking");
+      let i  = setInterval(() => {
+        console.log("this will run every second");
+      }, 1000);
+      setTickInterval(i);
+      console.log("setting tick interval to", i);
+      setTicking(true);
+    } else {
+      console.log("turning off ticking");
+      console.log("turning off tickInterval", tickInterval);
+      setTickInterval(null);
+      clearInterval(tickInterval);
+      setTicking(false);
+    }
+  }
 
   return (
     <div className="container">
@@ -99,6 +134,7 @@ function App() {
           </nav>
         </div>
         <div className="col-md-10">
+          <a className="btn btn-outline-secondary" href="#!" onClick={toggleRefresh}>Toggle Ticking</a>
           <Alert message={alertMessage} className={alertClassName} />
           <Outlet
             context={{
